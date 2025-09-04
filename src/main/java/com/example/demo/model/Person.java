a8d430e50ac765e731b2459c151f74f206e5ce88
danishahsancs <danishahsancs@gmail.com>
2025-09-04 15:23:26 -0400

    finished person,java

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,25 @@
+package main.java.com.example.demo.model;
+
+public class Person {
+
+    final private long id;
+    private String name;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
