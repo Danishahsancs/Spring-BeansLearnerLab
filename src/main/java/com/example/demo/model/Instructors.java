b26272d8806676c1aaf8de04c9c0ea826662c662
danishahsancs <danishahsancs@gmail.com>
2025-09-04 16:09:33 -0400

    instructors

---
+++ src/main/java/com/example/demo/model/Instructors.java
@@ -0,0 +1,7 @@
+package main.java.com.example.demo.model;
+
+public class Instructors extends People<Instructor> {
+    public Instructors(Instructor... instructors) {
+        super(instructors);
+    }
+}
